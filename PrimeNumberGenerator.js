d5bc56fc008024b898805ed482e728ab547a92d8
kevinsounthavong <soun042564@gmail.com>
2022-04-05 17:10:40 -0400

    Modify isPrime to handle odd composite numbers

--- PrimeNumberGenerator.js
+++ PrimeNumberGenerator.js
@@ -9,6 +9,11 @@ class PrimeNumberGenerator {
 		if (num === 2) return true;
 		if (num % 2 === 0) return false;
 
+		// check if number is odd and composite
+		for (let i = 3; i <= Math.sqrt(num); i += 2) {
+			if (num % i === 0) return false;
+		}
+
 		return true;
 	}
 
