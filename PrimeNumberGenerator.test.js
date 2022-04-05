b0066e265698cbb461d72d65556efd65b7317bbb
kevinsounthavong <soun042564@gmail.com>
2022-04-05 16:45:31 -0400

    Add isPrime tests for 1 and 0 as args

--- PrimeNumberGenerator.test.js
+++ PrimeNumberGenerator.test.js
@@ -11,6 +11,14 @@ describe('isPrime', () => {
 	test('should return true when argument is 2', () => {
 		expect(primeNumberGeneratorObj.isPrime(2)).toBe(true);
 	});
+
+	test('should return false when argument is 1', () => {
+		expect(primeNumberGeneratorObj.isPrime(1)).toBe(false);
+	});
+
+	test('should return false when argument is 0', () => {
+		expect(primeNumberGeneratorObj.isPrime(0)).toBe(false);
+	});
 });
 
 describe('generate', () => {});
