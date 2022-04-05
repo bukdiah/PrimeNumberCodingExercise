042094ebb26b217a1754e797bda597f2d89a7c31
kevinsounthavong <soun042564@gmail.com>
2022-04-05 09:11:28 -0400

    Add PrimeNumberGenerator class and empty tests

---
+++ PrimeNumberGenerator.js
@@ -0,0 +1,16 @@
+// JS does not have interfaces
+// creating class instead
+
+class PrimeNumberGenerator {
+	constructor() {}
+
+	isPrime(num) {
+		return;
+	}
+
+	generate(startingValue, endingValue) {
+		return;
+	}
+}
+
+module.exports = PrimeNumberGenerator;
---
+++ PrimeNumberGenerator.test.js
@@ -0,0 +1,8 @@
+const { test, expect, describe } = require('@jest/globals');
+const PrimeNumberGenerator = require('./PrimeNumberGenerator');
+
+const primeNumberGeneratorObj = new PrimeNumberGenerator();
+
+describe('isPrime', () => {});
+
+describe('generate', () => {});
