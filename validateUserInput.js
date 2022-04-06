cf4ea879462dfca7d63ff8df4ec10de8bc148ae3
kevinsounthavong <soun042564@gmail.com>
2022-04-05 22:02:11 -0400

    Make validateUserInput detect positive integers

--- validateUserInput.js
+++ validateUserInput.js
@@ -1,4 +1,8 @@
 function validateUserInput(input) {
+	const num = Number(input); // convert string to number..empty string returns 0
+
+	// if num is a positive integer
+	if (Number.isInteger(num) && num > 0) return true;
 	return false;
 }
 
