ac3c409c5727924a41e93ec057f45eaac9f58a8d
kevinsounthavong <soun042564@gmail.com>
2022-04-05 22:19:59 -0400

    Add validateUserInput tests for alphanum strings

--- validateUserInput.test.js
+++ validateUserInput.test.js
@@ -12,3 +12,7 @@ test('should return true when given a positive integer like 3', () => {
 test('should return false when given a number with a decimal point like 2.0', () => {
 	expect(validateUserInput('2.0')).toBe(false);
 });
+
+test('should return false when given a string with letters and numbers like 123abc', () => {
+	expect(validateUserInput('123abc')).toBe(false);
+});
